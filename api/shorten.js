40a2b68d791c9f680237575346e4cb23205771c2
Adam Sisk <adamsisk91@gmail.com>
2019-10-14 22:50:00 -0500

    res messages clarified

--- api/shorten.js
+++ api/shorten.js
@@ -26,7 +26,7 @@ router.get('/:slug', async (req, res, next) => {
       res.json({ redirect: redirect.redirect })
     } else {
       // slug doesn't exist
-      res.json({ message: 'Custom name not found' })
+      res.json({ message: 'expired or incorrect link' })
     }
   } catch (err) {
     next(err);
