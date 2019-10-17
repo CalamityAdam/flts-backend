a772e885f843d2d79de5ba1133d4e7a6a38581aa
Adam Sisk <adamsisk91@gmail.com>
2019-10-16 23:22:31 -0500

    socket removed, idk why i added it to begin with...

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = require('./db');
 const sessionStore = new SequelizeStore({ db });
 const PORT = process.env.PORT || 4000;
 const app = express();
-const socketio = require('socket.io');
+// const socketio = require('socket.io');
 module.exports = app;
 
 if (process.env.NODE_ENV !== 'production') require('./secrets');
@@ -81,8 +81,8 @@ const startListening = () => {
   );
 
   // set up our socket control center
-  const io = socketio(server);
-  require('./socket')(io);
+  // const io = socketio(server);
+  // require('./socket')(io);
 };
 
 const syncDb = () => db.sync();
