65700e46717c8611db1156546541648875cb0627
Adam Sisk <adamsisk91@gmail.com>
2019-10-19 21:51:28 -0500

    prevent headers from attempt to set after response sent

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const createApp = () => {
     },
   };
   if (app.get('env') === 'production') {
-    app.set('trust proxy', 1);
+    // app.set('trust proxy', 1);
     sess.cookie.secure = true;
   };
   app.use(
