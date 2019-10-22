ffe76f16e8e6ad608f110de386cfebc7464ae5d2
Adam Sisk <adamsisk91@gmail.com>
2019-10-21 23:22:52 -0500

    console db sync remove force

--- console.js
+++ console.js
@@ -3,8 +3,8 @@ const { Shorten, User } = require('./db/models');
 const db = require('./db');
 
 // uncomment this to wipe DB
-db.sync({ force: true });
-// db.sync();
+// db.sync({ force: true });
+db.sync();
 
 const replServer = repl.start({
   prompt: 'FTLS > ',
